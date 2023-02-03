ff376b55c9f5e44e8bf091b5a1b91c718a41a045
marianaohde <91140198+marianaohde@users.noreply.github.com>
2023-02-03 20:05:10 +0000

    Update app.py

--- Lab 3/app.py
+++ Lab 3/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 
- #Lab 3
-from flask import render_template, request
 @app.route("/form", methods=["GET", "POST"])
 def form():
     if request.method == "POST":
@@ -10,16 +14,9 @@ def form():
         return "Hello " + name
     return render_template("index.html")
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
 class NameForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-
-from flask import render_template, request
 
 @app.route("/form", methods=["GET", "POST"])
 def form():
@@ -31,7 +28,6 @@ def form():
 
 app.config["SECRET_KEY"] = "your_secret_key"
 
-from flask_wtf.csrf import CSRFProtect
 csrf = CSRFProtect(app)
 
 app.run()
